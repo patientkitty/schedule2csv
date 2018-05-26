0e49090bcdbdc88e58dc9636f13f05a93945107a
Zhifeng Shen <ss9545@NYUSH1499LP-MX.local>
2018-05-26 16:52:34 +0800

    input view init

--- resources/views/input.blade.php
+++ resources/views/input.blade.php
@@ -4,6 +4,29 @@
     Hello World
     <br>
     <a href="#" class="btn btn-primary">This is a button</a>
+    <table class="table">
+        <thead>
+        <tr>
+            <th scope="col">Group</th>
+            <th scope="col">Course Name</th>
+            <th scope="col">Date Start</th>
+            <th scope="col">Date End</th>
+            <th scope="col">Time Start</th>
+            <th scope="col">Time End</th>
+            <th scope="col">Week Day</th>
+            <th scope="col">Classroom</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr>
+            <td><input type="text" class="form-control" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1">
+                </td>
+            <td>Mark</td>
+            <td>Otto</td>
+            <td>@mdo</td>
+        </tr>
+        </tbody>
+    </table>
 
 
 @endsection
