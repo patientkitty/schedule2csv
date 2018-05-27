b1a6f31448555e8c2cd3f5124c6a2a90d75c62c5
patientkitty <13916890341@163.com>
2018-05-27 21:49:06 +0800

    complete add schedule function

--- app/Http/Controllers/scheduleController.php
+++ app/Http/Controllers/scheduleController.php
@@ -10,69 +10,69 @@ use App\Models\Weekday;
 class scheduleController extends Controller
 {
     //
-    public function newschedule(){
-//        $weekday = Weekday::where('id','<',11)->get();
-//        dd($weekday);
-        //从网页表单获取课程信息
-        $group = 1;
-        $eventname = 'test class';
-        $datestart = '2018-06-01';
-        $dateend = '2018-09-11';
-        $timestart = 1100;
-        $timeend = 1305;
-        $weekdays = 3;
-        $room = 210;
-        $data = [
-            'group'=>1,
-            'event_name'=>'test class',
-            'date_start'=>'2018-06-01',
-            'date_end'=>'2018-07-11',
-            'time_start'=>1100,
-            'time_end'=>1305,
-            'weekdays'=>3,
-            'room'=>210
-                ];
+    public function newschedule($data){
 
-//        //插入schedules表，记录课程每周几上课
-//        $schedule = new Schedule();
-//        foreach ($data as $key => $value) {
-//            $schedule->$key = $data[$key];
-//        }
-//        $schedule->save();
-        //dd($data);
-        //查询weekdays表，获取到具体上课日期
-        $exportdate = Weekday::where([
-            ['date','>=',$data['date_start']],
-            ['date','<=',$data['date_end']],
-            ['weekday','=',$data['weekdays']],
+        //从网页表单inputs获取课程信息$data，检查在schedule表中是否存在重复数据
+        $existSchedule = Schedule::where([
+            ['event_name','=',$data['event_name']],
+            ['date_start','=',$data['date_start']],
+            ['date_end','=',$data['date_end']],
+            ['time_start','=',$data['time_start']],
+            ['time_end','=',$data['time_end']],
+            ['room','=',$data['room']],
+            ['weekdays','=',$data['weekdays']],
         ])->get();
-        //echo $exportdate;
-        //创建导出格式的数组
-        $exportdata =[];
-        foreach ($exportdate as $value){
-            //echo $value;
-            array_push($exportdata,[
-                'group'=>$data['group'],
-                'event_name'=>$data['event_name'],
-                'date'=>$value['date'],
-                'time_start'=>$data['time_start'],
-                'time_end'=>$data['time_end'],
-                'room'=>$data['room']
-            ]);
-        }
-        //插入exports表，用于EMS导出格式
+        if($existSchedule){
+            echo 'schedule already exist';
+        }else{
+            //dd($data);
+            //插入schedules表，记录课程每周几上课
+            $schedule = new Schedule();
+            foreach ($data as $key => $value) {
+                if($key != '_token'){
+                    $schedule->$key = $data[$key];
+                }
+            }
+            $schedule->save();
+            //dd($data);
+            //查询weekdays表，获取到具体上课日期
+            $exportdate = Weekday::where([
+                ['date','>=',$data['date_start']],
+                ['date','<=',$data['date_end']],
+                ['weekday','=',$data['weekdays']],
+            ])->get();
+            //echo $exportdate;
+            //创建导出格式的数组
+            $exportdata =[];
+            foreach ($exportdate as $value){
+                //echo $value;
+                array_push($exportdata,[
+                    'group'=>$data['group'],
+                    'event_name'=>$data['event_name'],
+                    'date'=>$value['date'],
+                    'time_start'=>$data['time_start'],
+                    'time_end'=>$data['time_end'],
+                    'room'=>$data['room']
+                ]);
+            }
+            //插入exports表，用于EMS导出格式
 //        $export = new Export();
 //        $export->truncate();
-        foreach ($exportdata as $dates) {
-            //print_r($dates);
-            $export = new Export();
-            foreach ($dates as $key => $value) {
-                //printf($dates['date']);
-                $export->$key = $dates[$key];
+            foreach ($exportdata as $dates) {
+                //print_r($dates);
+                $export = new Export();
+                foreach ($dates as $key => $value) {
+                    //printf($dates['date']);
+                    $export->$key = $dates[$key];
+                }
+                $export->save();
             }
-            $export->save();
         }
 
+
+    }
+    public function searchEventName(){
+        echo 'hello';
     }
 
     public function test(){
@@ -86,7 +86,9 @@ class scheduleController extends Controller
     public function add(Request $request){
         //$inputs = $request->input('Group');
         //return view('input',['inputs'=>$inputs]);
-        echo $request->input('group');
-        dd($request);
+        $inputs= $request->all();
+        $this->newschedule($inputs);
+        echo 'Add Complete';
+        //dd($inputs);
     }
 }
--- resources/views/input.blade.php
+++ resources/views/input.blade.php
@@ -11,31 +11,31 @@
             </div>
             <div class="form-group col-md-2">
                 <label for="inputCourseName">Course Name</label>
-                <input type="text" name="course" class="form-control" id="inputCourseName" placeholder="Event Name">
+                <input type="text" name="event_name" class="form-control" id="inputCourseName" placeholder="Event Name">
             </div>
             <div class="form-group col-md-2">
                 <label for="inputDateStart">Date Start</label>
-                <input type="text" name="datestart" class="form-control" id="inputDateStart" placeholder="yyyy-mm-dd">
+                <input type="text" name="date_start" class="form-control" id="inputDateStart" placeholder="yyyy-mm-dd">
             </div>
             <div class="form-group col-md-2">
                 <label for="inputDateEnd">Date End</label>
-                <input type="text" name="dateend" class="form-control" id="inputDateEnd" placeholder="yyyy-mm-dd">
+                <input type="text" name="date_end" class="form-control" id="inputDateEnd" placeholder="yyyy-mm-dd">
             </div>
             <div class="form-group col-md-1">
                 <label for="inputTimeStart">Time Start</label>
-                <input type="text" name="timestart" class="form-control" id="inputTimeStart" placeholder="hhmm">
+                <input type="text" name="time_start" class="form-control" id="inputTimeStart" placeholder="hhmm">
             </div>
             <div class="form-group col-md-1">
                 <label for="inputTimeEnd">Time End</label>
-                <input type="text" name="timeend" class="form-control" id="inputTimeEnd" placeholder="hhmm">
+                <input type="text" name="time_end" class="form-control" id="inputTimeEnd" placeholder="hhmm">
             </div>
             <div class="form-group col-md-1">
                 <label for="inputWeekday">Weekday</label>
-                <input type="text" name="weekday" class="form-control" id="inputWeekday" placeholder="#">
+                <input type="text" name="weekdays" class="form-control" id="inputWeekday" placeholder="#">
             </div>
             <div class="form-group col-md-1">
                 <label for="inputClassroom">Classroom</label>
-                <input type="text" name="classroom" class="form-control" id="inputClassroom" placeholder="#">
+                <input type="text" name="room" class="form-control" id="inputClassroom" placeholder="#">
             </div>
         </div>
 
